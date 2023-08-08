e8d0b19fe49857aded409be6b9356dbf126e35f4
Anonymous15110 <paviachar1505@gmail.com>
2023-08-08 21:59:48 +0530

    Update endpoints.js

--- endpoints.js
+++ endpoints.js
@@ -69,12 +69,6 @@ const fs = require('fs');
 
 app.post('/writetofile', (req, res) => {
     const content = req.body.content;
-    fs.writeFile('EU3.txt', content, err => {
-        if (err) {
-            console.error(err);
-        }
-        // file written successfully
-    });
     console.log("CHeck");
 })
 
